50f008476555fb908117fa8dfb11a327c5dc3358
datnguyen18 <dat.n8n@gmail.com>
2022-09-27 23:02:53 +0700

    update exercise lab 6.4

--- selenium-labs/src/com/selenium/lab6/Exercise4.java
+++ selenium-labs/src/com/selenium/lab6/Exercise4.java
@@ -3,11 +3,15 @@ package com.selenium.lab6;
 import java.util.Scanner;
 
 public class Exercise4 {
+    /*
+    * https://google.com
+    *
+    * */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Input your URL: ");
         String url = sc.nextLine();
-        String schema = url.split(":")[0];
+        String schema = url.substring(0, url.indexOf(":"));
         String topLevelDomain = url.split("\\.")[1];
         String subDomain = url.replace("." + topLevelDomain, "");
         subDomain = subDomain.replace(schema+ "://", "");
